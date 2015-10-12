70261eea1777ab1d3e39646d045090db082e5a2e
edogmonkey <Edogmonkey@gmail.com>
2015-10-12 15:40:25 -0400

    AutoMode

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutoOpv1.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutoOpv1.java
@@ -2,7 +2,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
+
 
 /**
  * Created by edogmonkey on 10/8/15.
@@ -10,27 +10,34 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 public class AutoOpv1 extends LinearOpMode
 {
 
-    public DcMotor leftMotor;
-    public DcMotor rightMotor;
-
-    @Override
+    DcMotor leftMotor;
+    DcMotor rightMotor;
     public void runOpMode() throws InterruptedException
     {
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");//Set the leftMotor to the motor in the hardware map
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");//Set the rightMotor to the motor in  the hardware map
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);//Reverse the right motor
 
-        for(int i=0; i<4; i++) {
+        waitForStart();
+
+        int a = 0;
+
+        do
+        {
             leftMotor.setPower(1.0);
             rightMotor.setPower(1.0);
 
-            sleep(1000);
+            sleep(500);
 
             leftMotor.setPower(0.5);
             rightMotor.setPower(-0.5);
 
             sleep(500);
-        }
 
-        leftMotor.setPowerFloat();
-        rightMotor.setPowerFloat();
+            a++;
+        }while(a<4);
+
+
 
     }
 }
