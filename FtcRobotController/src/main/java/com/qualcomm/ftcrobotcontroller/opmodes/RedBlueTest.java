32a19a4f300ee197f8e2b43ffbf9b429da184879
theonlyones <anthonysalvato@foresthills.edu>
2015-12-05 15:31:12 -0500

    New stuff

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/RedBlueTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/RedBlueTest.java
@@ -2,21 +2,23 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 
 public class RedBlueTest extends LinearOpMode{
     @Override
     public void runOpMode() throws InterruptedException {
         Drive myDrive;
-        boolean red = true;
+        TouchSensor Tsensor;
+        Tsensor = hardwareMap.touchSensor.get("touch");
+        boolean red = !Tsensor.isPressed();
 
         myDrive = new Drive(hardwareMap.dcMotor.get("motor_1"),hardwareMap.dcMotor.get("motor_2"), red);
 
         waitForStart();
 
         sleep(10000);
-       myDrive.backward(72,0.5);
+       myDrive.backward(72, 0.5);
 
         myDrive.pointturn(90,0.25);
         //myDrive.pointturn(180,0.25);
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TouchTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TouchTest.java
@@ -39,5 +39,6 @@ public class TouchTest extends OpMode {
 
 
         }
+
     }
 }
