d7d70247f2cd6ff4832ca6df1af93451d38fbe3d
Edogmonkey <Edogmonkey@gmail.com>
2015-12-09 14:37:41 -0500

    FullUserControl added

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankDrive2.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TankDrive2.java
@@ -5,71 +5,47 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
- * Created by Cole Salvato on 10/3/2015.
+ * Created by Cole Salvato on 10/3/2015. What a
  */
-public class TankDrive extends OpMode {
-    boolean motorFast = false;
+public class TankDrive2 extends OpMode {
+
     DcMotor lift;
     DcMotor right;
     DcMotor left;
     DcMotor twist;
-    Servo servo1;
-    Servo servo2;
-    double spinPos = 0.5;
-    double rightPower = 0;
-    double leftPower = 0;
+    Servo servo;
+    Servo spin;
+
     public void init()
     {
-        servo2 = hardwareMap.servo.get("spin");
-        servo1 = hardwareMap.servo.get("servo");
+        servo = hardwareMap.servo.get("servo");
+        spin = hardwareMap.servo.get("spin");
         right = hardwareMap.dcMotor.get("motorRight");
         left = hardwareMap.dcMotor.get("motorLeft");
         twist = hardwareMap.dcMotor.get("motorTwist");
         lift = hardwareMap.dcMotor.get("motorLift");
         right.setDirection(DcMotor.Direction.REVERSE);
-
     }
     public void loop()
     {
-        /*
-        if(gamepad1.b = true || motorFast == true)
-        {
-            rightPower = (-gamepad1.right_stick_y * 0.75);
-            leftPower = (-gamepad1.left_stick_y * 0.75);
-            motorFast = true;
-        }
-        if(gamepad1.a = true || motorFast == false)
-        {
-          rightPower = (-gamepad1.right_stick_y * 0.33);
-            leftPower = (-gamepad1.left_stick_y * 0.33);
-            motorFast = false;
-        }
-        if(gamepad1.left_bumper == true){
-            leftPower = leftPower - 0.25;
-        }
-        if(gamepad1.right_bumper == true){
-            rightPower = rightPower - 0.25;
-        }
-        if(gamepad1.left_trigger > 0.2){
-            leftPower = leftPower + 0.25;
-        }
-        if(gamepad1.right_trigger > 0.2){
-            rightPower = rightPower + 0.25;
-        }*/
         right.setPower(-gamepad1.right_stick_y * 0.75);
         left.setPower(-gamepad1.left_stick_y * 0.75);
         twist.setPower(gamepad2.left_stick_x * 0.25);
-        lift.setPower(gamepad2.left_stick_y * 0.4 + 0.25);
-        if (gamepad2.left_trigger > 0.1)
-            spinPos = spinPos -0.03;
-        else if (gamepad2.right_trigger >0.1)
-        spinPos = spinPos +0.03;
-        if(spinPos>1)
-            spinPos = 1;
-        if(spinPos<0)
-            spinPos = 0;
-        servo1.setPosition(spinPos);
-        servo2.setPosition(gamepad2.right_stick_y/2 + 0.5);
+        if (-gamepad2.right_stick_y > 0)
+            lift.setPower(-gamepad2.right_stick_y * 0.6 - 0.25);
+        if (gamepad2.left_trigger > 0){
+            servo.setDirection(Servo.Direction.FORWARD);
+            servo.setPosition(gamepad2.left_trigger);}
+        else if (gamepad2.right_trigger > 0){
+            servo.setDirection(Servo.Direction.REVERSE);
+            servo.setPosition(gamepad2.right_trigger);}
+        else
+            servo.setPosition(0);
+        if (gamepad2.right_bumper)
+            spin.setDirection(Servo.Direction.FORWARD);
+        else
+            spin.setDirection(Servo.Direction.REVERSE);
+        spin.setPosition(1);
     }
     public void stop(){}
 }
